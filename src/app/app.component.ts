dd93834f854cd3f170dfdec06fdc228c66c0bfbd
Jordan Woodruff <jordan@neko.local>
2017-04-17 20:22:42 -0500

    Angular App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  list: string[] = [];
+  item: string = "new list";
+
+  add() : void {
+    this.list.push(this.item);
+  }
 }
