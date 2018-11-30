2af11212b43bdc0dda976382ab6b4a8638013a94
Jordan Woodruff <jordan@neko-chan.local>
2018-11-29 18:59:35 -0600

    login html and css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GrocListComponent } from './groc-list/groc-list.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
     { path: '', component: GrocListComponent },
+    { path: 'login', component: LoginComponent},
     { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 @NgModule ({
