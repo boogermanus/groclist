d6c1eaeaea178a556d7f36cee60781295bd33bf4
Jordan Woodruff <jordan@neko-chan.local>
2018-11-29 18:32:26 -0600

    lint issue

--- src/app/login-component/login-component.component.ts
+++ src/app/login-component/login-component.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login-component',
   templateUrl: './login-component.component.html',
-  styleUrls: ['./login-component.component.css']
+  styleUrls: ['./login-component.component.css'],
 })
 export class LoginComponentComponent implements OnInit {
 
