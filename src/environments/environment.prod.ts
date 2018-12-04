c4762900e85f482b266ab981adfcaf398c23e516
Jordan Woodruff <jordan@neko-chan.local>
2018-12-03 19:32:39 -0600

    remove prod

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  groceryListAPI: 'https://api.permutate.us:8444/api/groceryLists',
-  groceryListItemAPI: 'https://api.permutate.us:8444/api/groceryListItems',
-};
