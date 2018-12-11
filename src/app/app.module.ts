3521fa87ce1a79fe5f9b80c85aed08bb76d1e003
Jordan Woodruff <jordan@neko-chan.local>
2018-12-10 19:59:19 -0600

    jw-interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import { GrocListModule } from './groc-list/groc-list.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import {CommonModule} from '@angular/common';
 import { LoginService } from './login/login.service';
 import { AuthGuardService } from './login/auth-guard.service';
 import { JwtHelperService, JwtModule } from '@auth0/angular-jwt';
+import {JwtInterceptor} from './login/jwt-interceptor';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,7 @@ import { JwtHelperService, JwtModule } from '@auth0/angular-jwt';
       },
     }),
   ],
-  providers: [ LoginService, AuthGuardService ],
+  providers: [ LoginService, AuthGuardService, {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
