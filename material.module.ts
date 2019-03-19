c22f8dfc4e9c6950a7a60086e92461daf7196349
Jordan Woodruff <jordan@neko-chan.local>
2019-03-19 18:51:02 -0500

    add material module

---
+++ material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { MatFormField, MatInput} from '@angular/material'
+@NgModule({
+    imports: [MatFormField, MatInput],
+    exports: [MatFormField, MatInput],
+})
+export class MaterialModule {
+
+}
