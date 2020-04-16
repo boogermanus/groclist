593157c2bd98c571a63604ce19a8f62d67127f5f
jordan.woodruff <Jordan.Woodruff@tylertech.com>
2020-04-16 14:27:20 -0500

    add remaining packages and build

--- groclist-api/src/app.service.ts
+++ groclist-api/src/app.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import * as shortid from 'shortid';
 @Injectable()
 export class AppService {
+  public static instanceId: string = shortid.generate();
   getHello(): string {
     return 'Hello World!';
   }
