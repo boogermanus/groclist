3719aa457a1a4b0cba23cd38d17a90349b41706c
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-10-20 00:42:44 -0500

    tying to fix tests

--- groclist/src/app/login/login.component.spec.ts
+++ groclist/src/app/login/login.component.spec.ts
@@ -1,6 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormBuilder } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LoginComponent } from './login.component';
+import { LoginService } from './login.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -9,6 +12,8 @@ describe('LoginComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
+      imports: [HttpClientTestingModule],
+      providers: [FormBuilder, LoginService]
     })
     .compileComponents();
   }));
