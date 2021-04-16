9fa6a8c964df91e11184fa8bb4710660132ca95d
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-04-15 23:33:39 -0500

    remove unused

--- groclist-api-dotnet/GrocListApi/Startup.cs
+++ groclist-api-dotnet/GrocListApi/Startup.cs
@@ -1,19 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using GrocListApi.Core.Models;
 using GrocListApi.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace GrocListApi
