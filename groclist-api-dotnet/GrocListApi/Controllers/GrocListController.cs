d8a6eb9b4e790ff13f5f1ae84419c1f32da8bc3b
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-04-15 19:31:12 -0500

    first controller

---
+++ groclist-api-dotnet/GrocListApi/Controllers/GrocListController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GrocListApi.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class GrocListController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok("GrocList API");
+        }
+    }
+}
