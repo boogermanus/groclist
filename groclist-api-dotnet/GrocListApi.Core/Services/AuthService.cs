3249e5d093653ea89af1d69236d49b9746dc6ae8
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-04-19 22:17:08 -0500

    adding methods for login

--- groclist-api-dotnet/GrocListApi.Core/Services/AuthService.cs
+++ groclist-api-dotnet/GrocListApi.Core/Services/AuthService.cs
@@ -1,9 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 using GrocListApi.Core.ApiModels;
 using GrocListApi.Core.Interfaces;
 using GrocListApi.Core.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace GrocListApi.Core.Services
 {
@@ -26,6 +31,34 @@ namespace GrocListApi.Core.Services
         public AuthModel Login(LoginModel model)
         {
             throw new System.NotImplementedException();
+        }
+
+        private async Task<User> AuthenticateUserAsync(string userName, string password)
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+            var validPassword = await _userManager.CheckPasswordAsync(user, password);
+
+            if (user != null && validPassword)
+                return user;
+
+            return null;
+        }
+
+        private string GenerateJsonWebToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                new Claim(JwtRegisteredClaimNames.NameId, user.Id)
+            };
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: credentials);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
