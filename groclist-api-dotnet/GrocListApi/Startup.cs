01530fc228e648125bc9d5225725eb6d57655df1
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-04-20 19:47:36 -0500

    change url and finish auth startup

--- groclist-api-dotnet/GrocListApi/Startup.cs
+++ groclist-api-dotnet/GrocListApi/Startup.cs
@@ -1,7 +1,9 @@
+using System.Text;
 using GrocListApi.Core.Interfaces;
 using GrocListApi.Core.Models;
 using GrocListApi.Core.Services;
 using GrocListApi.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -9,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace GrocListApi
 {
@@ -21,8 +24,7 @@ namespace GrocListApi
         {
             Configuration = configuration;
         }
-
-
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -32,6 +34,23 @@ namespace GrocListApi
                 
             services.AddIdentity<User, IdentityRole>()
                 .AddEntityFrameworkStores<AppDbContext>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
 
             services.AddScoped<IAuthService, AuthService>();
         }
@@ -44,10 +63,11 @@ namespace GrocListApi
                 app.UseDeveloperExceptionPage();
             }
 
+            // not needed in my hosting environment
             // app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
