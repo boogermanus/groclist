1110d60ae40e36882638f81f4029568551326acb
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-05-07 19:26:07 -0500

    add grocery list items table

--- groclist-api-dotnet/GrocListApi.Infrastructure/Migrations/20210508001946_GroceryListItem.Designer.cs
+++ groclist-api-dotnet/GrocListApi.Infrastructure/Migrations/20210508001946_GroceryListItem.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using GrocListApi.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GrocListApi.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210508001946_GroceryListItem")]
+    partial class GroceryListItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ groclist-api-dotnet/GrocListApi.Infrastructure/Migrations/20210508001946_GroceryListItem.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GrocListApi.Infrastructure.Migrations
+{
+    public partial class GroceryListItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GroceryListItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    IsCollected = table.Column<bool>(nullable: false),
+                    HasCoupon = table.Column<bool>(nullable: false),
+                    GroceryListId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroceryListItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GroceryListItems_GroceryList_GroceryListId",
+                        column: x => x.GroceryListId,
+                        principalTable: "GroceryList",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroceryListItems_GroceryListId",
+                table: "GroceryListItems",
+                column: "GroceryListId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GroceryListItems");
+        }
+    }
+}
