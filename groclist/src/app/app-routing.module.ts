368994512fc821314a3163c759a853ff69eaa731
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-08-19 15:44:07 -0500

    new routes

--- groclist/src/app/app-routing.module.ts
+++ groclist/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { GrocListComponent } from './groc-list/groc-list.component';
 import { LoginComponent } from './auth/login/login.component';
 import { AuthGuardService as AuthGuard} from './auth/auth-guard.service';
+import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
     { path: '', component: GrocListComponent, canActivate: [AuthGuard]},
     { path: 'login', component: LoginComponent},
+    { path: 'register', component: RegisterComponent},
     { path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
 @NgModule ({
