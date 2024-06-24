52186fc424955552367b90f93087983b6ad5ea99
Jordan <boogermanus@gmail.com>
2024-06-24 16:54:56 -0500

    starting over

--- groclist/src/app/auth/auth-model.ts
+++
@@ -1,9 +0,0 @@
-export class AuthModel {
-    public username: string;
-    public password: string;
-
-    constructor(pUsername: string, pPassword: string) {
-        this.username = pUsername;
-        this.password = pPassword;
-    }
-}
