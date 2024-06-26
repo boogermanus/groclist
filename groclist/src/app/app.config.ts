e21d7e59e3780eea4a34e8cff04f64decc927e5d
Jordan <boogermanus@gmail.com>
2024-06-26 11:54:32 -0500

    interceptor

--- groclist/src/app/app.config.ts
+++ groclist/src/app/app.config.ts
@@ -3,14 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
+import { jwtInterceptor } from './auth/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([jwtInterceptor])),
     { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
     JwtHelperService
   ]
