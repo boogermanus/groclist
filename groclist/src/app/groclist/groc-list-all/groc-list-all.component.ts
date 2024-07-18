4ffb2226ad8a137cb1eb921cef566bf0bbab664a
Jordan Woodruff <boogermanus@gmail.com>
2024-07-18 17:07:08 -0500

    table start

--- groclist/src/app/groclist/groc-list-all/groc-list-all.component.ts
+++ groclist/src/app/groclist/groc-list-all/groc-list-all.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table'
+import { GroceryListService } from '../../services/grocery-list.service';
+import { IGroceryList } from '../../interfaces/igrocery-list';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-groc-list-all',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule,
+    CommonModule
+  ],
   templateUrl: './groc-list-all.component.html',
   styleUrl: './groc-list-all.component.css'
 })
-export class GrocListAllComponent {
+export class GrocListAllComponent implements OnInit {
+
+
+  public displayedColumns: string[] = ['id']
+  public dataSource: MatTableDataSource<IGroceryList>;
+  constructor(
+    private readonly grocListService: GroceryListService
+  )
+  {}
+
+  ngOnInit(): void {
+    this.grocListService.getLists().subscribe(
+      {
+        next: (data) => {
+          this.dataSource = new MatTableDataSource<IGroceryList>(data);
+        }
+      }
+    )
+  }
 
 }
