8a6d5ac8b24b82121a5e31417288d86b9ed34a9b
Jordan Woodruff <boogermanus@gmail.com>
2024-07-20 15:51:15 -0500

    add checkbox

--- groclist/src/app/groclist/groc-list-all/groc-list-all.component.ts
+++ groclist/src/app/groclist/groc-list-all/groc-list-all.component.ts
@@ -4,13 +4,15 @@ import { GroceryListService } from '../../services/grocery-list.service';
 import { IGroceryList } from '../../interfaces/igrocery-list';
 import { CommonModule } from '@angular/common';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @Component({
   selector: 'app-groc-list-all',
   standalone: true,
   imports: [
     MatTableModule,
     CommonModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatCheckboxModule
   ],
   templateUrl: './groc-list-all.component.html',
   styleUrl: './groc-list-all.component.css'
