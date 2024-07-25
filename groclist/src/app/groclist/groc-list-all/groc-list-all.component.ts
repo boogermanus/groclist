5b1850d76536a59edd29cc749e066643f33c76e6
Jordan Woodruff <boogermanus@gmail.com>
2024-07-25 12:34:12 -0500

    table with expand

--- groclist/src/app/groclist/groc-list-all/groc-list-all.component.ts
+++ groclist/src/app/groclist/groc-list-all/groc-list-all.component.ts
@@ -5,6 +5,10 @@ import { IGroceryList } from '../../interfaces/igrocery-list';
 import { CommonModule } from '@angular/common';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list'
+import { trigger, state, style, transition, animate } from '@angular/animations';
 @Component({
   selector: 'app-groc-list-all',
   standalone: true,
@@ -12,17 +16,37 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatTableModule,
     CommonModule,
     MatPaginatorModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
   ],
   templateUrl: './groc-list-all.component.html',
-  styleUrl: './groc-list-all.component.css'
+  styleUrl: './groc-list-all.component.css',
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed,void', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class GrocListAllComponent implements OnInit {
 
 
-  public displayedColumns: string[] = ['id', 'name', 'createdDate', 'items', 'isComplete']
+  public displayedColumns: string[] = [
+    'id', 
+    'name', 
+    'createdDate', 
+    'items', 
+    'isComplete',
+    'expand'
+  ]
+
   public dataSource: MatTableDataSource<IGroceryList>;
   @ViewChild(MatPaginator, {static: true})paginator: MatPaginator;
+  public expandedElement: IGroceryList | null;
+
   constructor(
     private readonly grocListService: GroceryListService
   )
