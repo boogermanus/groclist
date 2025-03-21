855c78ff65c039846f281be447b929d1664e0c9d
Jordan Woodruff <boogermanus@gmail.com>
2025-03-21 14:27:45 -0500

    did not work

--- groclist/src/app/auth/jwt.interceptor.ts
+++ groclist/src/app/auth/jwt.interceptor.ts
@@ -1,28 +1,14 @@
-import { HttpInterceptorFn, HttpResponse } from '@angular/common/http';
-// import { tap } from 'rxjs';
-// import { inject } from '@angular/core';
-// import { Router } from '@angular/router';
+import { HttpInterceptorFn } from '@angular/common/http';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
-  if(token) {
+  if (token) {
     req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
   }
-
-  // trying to figure out how to redirect on invalid auth.
-  // return next(req).pipe(tap(event => {
-  //   if(event instanceof HttpResponse) {
-  //     if(event.status === 401) {
-  //       localStorage.clear();
-  //       inject(Router).navigate(['/login']);
-  //     }
-  //   }
-  //   return event;
-  // }));
 
   return next(req);
 };
