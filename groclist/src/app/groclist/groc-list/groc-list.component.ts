37ffcd3b8560ad4b6125188bc5314828535a4f28
Jordan Woodruff <boogermanus@gmail.com>
2025-04-11 16:11:36 -0500

    onSelected

--- groclist/src/app/groclist/groc-list/groc-list.component.ts
+++ groclist/src/app/groclist/groc-list/groc-list.component.ts
@@ -93,4 +93,8 @@ export class GrocListComponent implements OnDestroy, AfterContentInit, OnInit {
   public view(list: IGroceryList): void {
     this.router.navigate(['/list', list.id]);
   }
+
+  public onSelected(): void {
+    this.add();
+  }
 }
