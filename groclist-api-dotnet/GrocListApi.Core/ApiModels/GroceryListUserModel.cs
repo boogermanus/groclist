46d7be709e1ac5ca1bc41ae4c11878592aadf426
Jordan Woodruff <jwoodruff@datascan.com>
2026-01-20 17:57:10 -0600

    #41 required

--- groclist-api-dotnet/GrocListApi.Core/ApiModels/GroceryListUserModel.cs
+++ groclist-api-dotnet/GrocListApi.Core/ApiModels/GroceryListUserModel.cs
@@ -6,7 +6,7 @@ public class GroceryListUserModel
 {
     public int Id { get; set; }
     public required string Username { get; set; }
-    public int GroceryListId { get; set; }
+    public required int GroceryListId { get; set; }
     public string UserId { get; set; } = string.Empty;
 
     public GroceryListUser ToDomainModel()
